6eb130713c9b810190e97e18495e4e2b66d97194
Michael Escobar <escobarmichael16@gmail.com>
2021-05-18 16:38:15 -0500

    Estilos finalizados, empezando js para validar formulario

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,36 @@
-function recogerDatos(){
+const formulario = document.getElementById("formularioo");
+const inputs = document.querySelectorAll('#formularioo input');
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+const ValidarFormulario = () =>{
+
+;}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', ValidarFormulario  )
+	input.addEventListener('blur', ValidarFormulario  )
+});
+
+
+
+formulario.addEventListener('submit', (e) =>{
+    e.preventDefault();
+});
+
+
+
+
+
+
+
+/* function recogerDatos(){
 
     let nombreUsuario = document.getElementById("nombreUsuario").value;
     localStorage.setItem("Nombre", nombreUsuario);
@@ -8,8 +40,7 @@ function recogerDatos(){
 
     let correoUsuario = document.getElementById("correoUsuario").value;
     localStorage.setItem("Correo", correoUsuario);
-    
     let celularUsuario = document.getElementById("celularUsuario").value;
     localStorage.setItem("Celular", celularUsuario);
-}
+} */
 
